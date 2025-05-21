2985b319a0b5c308ce880e9f079cefabf8960886
FedericoPacheco <fedepacheco2112@gmail.com>
2025-05-21 20:21:18 -0300

    Add comment on how to use multiple github accounts

--- src/entryPoint.ts
+++ src/entryPoint.ts
@@ -1,10 +1,18 @@
 import { moduleBFunction } from "./moduleB";
 
-// https://www.npmjs.com/package/gas-webpack-plugin
-
 export function entryPointFunction(): void {
   moduleBFunction();
   console.log("Entry Point Function");
 }
 
+// Don't forget to add this line. Otherwise, the function won't be exported to the global scope.
+// https://www.npmjs.com/package/gas-webpack-plugin
 (global as any).entryPointFunction = entryPointFunction;
+
+// Use multiple github accounts:
+// Source: https://stackoverflow.com/a/27407168
+// Make sure to run: git remote set-url origin https://<USERNAME>@github.com/<USERNAME>/<REMOTE-NAME>
+// Then, login on the browser.
+
+// General info:
+// https://developers.google.com/apps-script/guides/typescript
