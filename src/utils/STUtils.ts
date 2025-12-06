6c3a8d001dd2efa41d403ea1a39e109b081b1844
FedericoPacheco <fedepacheco2112@gmail.com>
2025-12-06 17:08:33 -0300

    refactor(STUtils): extract e1rm functions

--- src/utils/STUtils.ts
+++ src/utils/STUtils.ts
@@ -1,11 +1,20 @@
 export default class STUtils {
   public static computeE1RM(weight: number, bw: number, reps: number): number {
-    const totalWeight = weight + bw;
-    const epley = totalWeight * (1 + reps / 30) - bw;
-    const brzycki = (totalWeight * 36) / (37 - reps) - bw;
-    const berger =
-      totalWeight * (1 / (1.0261 * Math.pow(Math.E, -0.0262 * reps))) - bw;
-
+    const epley = STUtils.estimate1RMEpley(weight, bw, reps);
+    const brzycki = STUtils.estimate1RMBrzycki(weight, bw, reps);
+    const berger = STUtils.estimate1RMBerger(weight, bw, reps);
     return (epley + brzycki + berger) / 3;
+  }
+
+  private static estimate1RMBerger(weight: number, bw: number, reps: number) {
+    return (weight + bw) * (1 / (1.0261 * Math.pow(Math.E, -0.0262 * reps))) - bw;
+  }
+
+  private static estimate1RMBrzycki(weight: number, bw: number, reps: number) {
+    return ((weight + bw) * 36) / (37 - reps) - bw;
+  }
+
+  private static estimate1RMEpley(weight: number, bw: number, reps: number) {
+    return (weight + bw) * (1 + reps / 30) - bw;
   }
 }
