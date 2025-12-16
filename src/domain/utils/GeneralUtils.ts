52fcaa76bd3e3a8279fcebc769d63edf421d765e
FedericoPacheco <fedepacheco2112@gmail.com>
2025-12-16 15:17:36 -0300

    refactor(GeneralUtils): remove unused sum() method and slide methods

--- src/domain/utils/GeneralUtils.ts
+++ src/domain/utils/GeneralUtils.ts
@@ -12,41 +12,6 @@ export default class GeneralUtils {
     return A.map((row, idx) => row.concat(B[idx]));
   }
 
-  public static round(num: number, decimalPlaces: number = 2): number {
-    if (isNaN(num)) return num;
-    const factor = Math.pow(10, decimalPlaces);
-    return Math.round(num * factor) / factor;
-  }
-
-  public static sum(arr: number[]): number {
-    if (!Array.isArray(arr)) throw new Error("Input is not a valid array");
-    return GeneralUtils.round(arr.reduce((acc, curr) => acc + curr, 0));
-  }
-
-  public static average(arr: number[]): number {
-    if (!Array.isArray(arr)) throw new Error("Input is not a valid array");
-    if (arr.length === 0) return 0;
-    return GeneralUtils.round(
-      arr.reduce((acc, curr) => acc + curr, 0) / arr.length
-    );
-  }
-
-  public static movingAverage(arr: number[], windowSize: number = 3): number[] {
-    debugger;
-    if (!Array.isArray(arr)) throw new Error("Input is not a valid array");
-    if (windowSize <= 0) throw new Error("Window size must be greater than 0");
-
-    const result: number[] = [];
-    for (let i = 0; i < arr.length; i++) {
-      const start = Math.max(0, i - windowSize + 1);
-      const end = i + 1;
-      const window = arr.slice(start, end);
-      result.push(this.average(window));
-    }
-
-    return result;
-  }
-
   public static transpose<T>(A: T[][]): T[][] {
     if (!Array.isArray(A) || !Array.isArray(A[0]))
       throw new Error("Input is not a valid matrix");
@@ -106,14 +71,6 @@ export default class GeneralUtils {
     return finalData;
   }
 
-  public static median(arr: number[]): number {
-    const sorted = [...arr].sort((a1, a2) => a1 - a2);
-    const midIdx = Math.floor(sorted.length / 2);
-    const isOdd = sorted.length % 2 !== 0;
-    if (isOdd) return sorted[midIdx];
-    else return (sorted[midIdx - 1] + sorted[midIdx]) / 2;
-  }
-
   public static split<T>(arr: T[], size: number): T[][] {
     const chunks: T[][] = [];
     let remainder = [...arr];
@@ -124,6 +81,48 @@ export default class GeneralUtils {
       remainder = remainder.slice(size);
     }
     return chunks;
+  }
+
+  public static round(num: number, decimalPlaces: number = 2): number {
+    if (isNaN(num)) return num;
+    const factor = Math.pow(10, decimalPlaces);
+    return Math.round(num * factor) / factor;
+  }
+
+  public static div(dividend: number, divisor: number): number {
+    return Math.floor(dividend / divisor);
+  }
+
+  public static average(arr: number[]): number {
+    if (!Array.isArray(arr)) throw new Error("Input is not a valid array");
+    if (arr.length === 0) return 0;
+    return GeneralUtils.round(
+      arr.reduce((acc, curr) => acc + curr, 0) / arr.length
+    );
+  }
+
+  public static movingAverage(arr: number[], windowSize: number = 3): number[] {
+    debugger;
+    if (!Array.isArray(arr)) throw new Error("Input is not a valid array");
+    if (windowSize <= 0) throw new Error("Window size must be greater than 0");
+
+    const result: number[] = [];
+    for (let i = 0; i < arr.length; i++) {
+      const start = Math.max(0, i - windowSize + 1);
+      const end = i + 1;
+      const window = arr.slice(start, end);
+      result.push(this.average(window));
+    }
+
+    return result;
+  }
+
+  public static median(arr: number[]): number {
+    const sorted = [...arr].sort((a1, a2) => a1 - a2);
+    const midIdx = Math.floor(sorted.length / 2);
+    const isOdd = sorted.length % 2 !== 0;
+    if (isOdd) return sorted[midIdx];
+    else return (sorted[midIdx - 1] + sorted[midIdx]) / 2;
   }
 
   public static relativeFrequencies<T extends string | number | symbol>(
@@ -138,10 +137,6 @@ export default class GeneralUtils {
       frequencies[item as T] /= arr.length;
     });
     return frequencies;
-  }
-
-  public static div(dividend: number, divisor: number): number {
-    return Math.floor(dividend / divisor);
   }
 
   public static getHours(input: number[]): number {
